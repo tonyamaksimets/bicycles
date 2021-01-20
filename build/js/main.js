ac830c1f5777609f73cb4e2b498a6c17635537c1
tonyamaksimets <berg_antonina@mail.ru>
2021-01-20 14:47:37 +0300

    Создает блок 'Видео'

--- build/js/main.js
+++ build/js/main.js
@@ -50,3 +50,29 @@
     });
   }
 })();
+
+'use strict';
+
+(function () {
+  var video = document.querySelector('video');
+  var controlButton = document.querySelector('.video__control');
+
+  var onControlButtonClick = function () {
+    video.setAttribute('controls', true);
+    video.play();
+    controlButton.classList.add('hidden');
+    video.addEventListener('pause', onVideoPause);
+  };
+
+  var onVideoPause = function () {
+    video.removeAttribute('controls');
+    controlButton.classList.remove('hidden');
+    video.removeEventListener('click', onVideoPause);
+  };
+
+  if (video && controlButton) {
+    video.removeAttribute('controls');
+
+    controlButton.addEventListener('click', onControlButtonClick);
+  }
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,8 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source//*.ico"
+    "source//*.ico",
+    "source/video/**"
     ], {
       base: "source"
     })
